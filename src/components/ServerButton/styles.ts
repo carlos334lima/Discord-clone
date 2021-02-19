5efc7627ecf4ce9650b70c85847c021093613590
Carlos Henrique de Lima <68131444+carlos334lima@users.noreply.github.com>
2021-02-18 22:30:23 -0300

    estilizando o server-button

--- src/components/ServerButton/styles.ts
+++ src/components/ServerButton/styles.ts
@@ -1,3 +1,17 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
-export const Button = styled.div``
+import { Props } from '.'
+
+export const Button = styled.button<Props>`
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-shrink:0; //Objeto não disponivel para minimizar
+
+    width: 48px;
+    height:48px;
+
+    margin-bottom:8px;
+
+`
