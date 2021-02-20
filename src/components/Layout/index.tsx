29b34b294a4e8a5f1ad1212907e44799c209b9a2
Carlos Henrique de Lima <68131444+carlos334lima@users.noreply.github.com>
2021-02-20 19:11:31 -0300

    Server Name adicionado

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,10 +3,12 @@ import React from 'react';
 import { Grid } from './style'
 
 import ServerList from '../ServerList/index'
+import ServerName from '../ServerName';
 
 const Layout: React.FC = () => {
     return (
         <Grid>
+            <ServerName />
             <ServerList />
         </Grid>
     )
